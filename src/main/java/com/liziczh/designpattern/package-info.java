50dc74aaee57ce61a3d87a5f5639f877d404002e
liziczh <liziczh@foxmail.com>
2022-01-17 00:01:51 +0800

    init

---
+++ src/main/java/com/liziczh/designpattern/package-info.java
@@ -0,0 +1,1 @@
+package com.liziczh.designpattern;
---
+++ src/main/java/com/liziczh/designpattern/singleton/Test.java
@@ -0,0 +1,12 @@
+package com.liziczh.designpattern.singleton;
+
+/**
+ * common
+ *
+ * @author chenzhehao
+ * @version 1.0
+ * @description
+ * @date 2022/1/17 12:00 上午
+ */
+public class Test {
+}
